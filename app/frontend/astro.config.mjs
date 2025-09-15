cd3f633d413214f9402d25573bd181cd7777b41d
Bryan Belandria <bryanbelandriav@gmail.com>
2025-09-15 04:33:15 -0400

    Including some important files

--- app/frontend/astro.config.mjs
+++ app/frontend/astro.config.mjs
@@ -1,22 +1,19 @@
-// app/frontend/astro.config.mjs
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'; // Import the node adapter
-import react from '@astrojs/react'; // If you use React
-import tailwindcss from '@astrojs/tailwind'; // If you use Tailwind
-import icon from 'astro-icon'; // If you use astro-icon
+import react from '@astrojs/react'; 
+import tailwindcss from '@astrojs/tailwind';
+import icon from 'astro-icon'; 
 
 export default defineConfig({
   // Add the node adapter for SSR
   adapter: node({
-    mode: 'middleware' // or 'lambda'
+    mode: 'middleware' 
   }),
   integrations: [
-    react(), // if you use React
-    icon(), // if you use astro-icon
+    react(),
+    icon(),
   ],
-  // Other Astro configurations...
   vite: {
     plugins: [tailwindcss()],
   },
-    // Make sure 'astro-icon' and other necessary plugins are correctly configured
 });
