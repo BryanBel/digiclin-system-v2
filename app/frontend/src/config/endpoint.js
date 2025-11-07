3cc57d41f9f6907fc40449e7404471e14a8b60ed
Bryan Belandria <bryanbelandriav@gmail.com>
2025-11-06 20:28:37 -0400

    replace backend url env

--- app/frontend/src/config/endpoint.js
+++ app/frontend/src/config/endpoint.js
@@ -1,4 +1,4 @@
 const DEFAULT_DEV_ENDPOINT = 'http://localhost:3000';
 
 export const BACK_ENDPOINT =
-	import.meta.env.PUBLIC_BACKEND_URL ?? (import.meta.env.DEV ? DEFAULT_DEV_ENDPOINT : '');
+	import.meta.env.BACKEND_URL ?? (import.meta.env.DEV ? DEFAULT_DEV_ENDPOINT : '');
