c7a3a87138bfa2c30589a8b90f869d5799bff85a
Bryan Belandria <bryanbelandriav@gmail.com>
2025-11-07 00:10:16 -0400

    Definitive edition

--- app/backend/src/services/nodemailer.js
+++ app/backend/src/services/nodemailer.js
@@ -1,14 +1,14 @@
- import { createTransport } from 'nodemailer';
+import { createTransport } from 'nodemailer';
 
 const nodemailerService = createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 587, // Puerto 587 con STARTTLS (más compatible con hostings)
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  requireTLS: true,
+  requireTLS: true, // Fuerza el uso de TLS
   pool: true,
   maxConnections: 1,
   rateDelta: 1000,
